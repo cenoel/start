4b3c5ac1d9441360323622a3992ecb5e5952fb5e
Razafimiandrisoa Noarison Léonce <trazafimiandrisoa@gmail.com>
2018-08-17 17:46:49 +0300

    modification view res partner

--- start/__manifest__.py
+++ start/__manifest__.py
@@ -20,6 +20,7 @@
         # 'security/ir.model.access.csv',
         'data/start_res_config_settings_data.xml',
         'data/start_type_partner_data.xml',
+        'views/start_res_partner_view.xml',
         'views/start_doctor_partner_view.xml',
         'views/start_patient_partner_view.xml',
         'views/start_maintenance_equipment_view.xml',
